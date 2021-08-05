494363f30b5401523621f86f43db677531809cc5
DenProgMan <rkden58@gmail.com>
2021-08-05 12:22:08 +0300

    feat: implement function towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 // You should implement your task here.
 
+function isOdd(num) {
+    return num === 0 || !!(num && (num%2));
+}
+
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix) return [];
+    let result = [];
+
+    for (const [ key, line ] of Object.entries(matrix)) {
+        result = result.concat(isOdd(key)
+            ? line.reverse()
+            : line);
+    }
+
+    return result;
 }
